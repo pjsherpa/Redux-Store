7152c22e50466dc0d4492236e299a592deca7622
Phinjock Sherpa <phinjock@gmail.com>
2022-11-21 11:46:04 -0800

    commented out codes and tries adding in redux codes in globalstate

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,23 +1,27 @@
 import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+// import { useProductReducer } from "./reducers";
+import { useDispatch, useSelector } from "react-redux";
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
+// const StoreProvider = ({ value = [], ...props }) => {
+//   const [state, dispatch] = useProductReducer({
+//     products: [],
+//     cart: [],
+//     cartOpen: false,
+//     categories: [],
+//     currentCategory: "",
+//   });
 
-  return <Provider value={[state, dispatch]} {...props} />;
-};
+//   return <Provider value={[state, dispatch]} {...props} />;
+// };
 
 const useStoreContext = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
   return useContext(StoreContext);
 };
 
-export { StoreProvider, useStoreContext };
+export { useStoreContext };
