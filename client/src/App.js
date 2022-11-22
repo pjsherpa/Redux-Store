6c22a5b954ec329d23b7e89b3d2d4a6c16e33dc5
Phinjock Sherpa <phinjock@gmail.com>
2022-11-21 18:33:30 -0800

    updated app.js,cart updated to redux, found where to update rest, commented out globalstate codes

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-import Home from './pages/Home';
-import Detail from './pages/Detail';
-import NoMatch from './pages/NoMatch';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
-import Success from './pages/Success';
-import OrderHistory from './pages/OrderHistory';
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Nav from "./components/Nav";
+import { Provider } from "react-redux";
+import Success from "./pages/Success";
+import OrderHistory from "./pages/OrderHistory";
+import store from "./utils/store";
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
 });
 
 function App() {
@@ -42,39 +36,18 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store={store}>
             <Nav />
             <Routes>
-              <Route 
-                path="/" 
-                element={<Home />} 
-              />
-              <Route 
-                path="/login" 
-                element={<Login />} 
-              />
-              <Route 
-                path="/signup" 
-                element={<Signup />} 
-              />
-              <Route 
-                path="/success" 
-                element={<Success />} 
-              />
-              <Route 
-                path="/orderHistory" 
-                element={<OrderHistory />} 
-              />
-              <Route 
-                path="/products/:id" 
-                element={<Detail />} 
-              />
-              <Route 
-                path="*" 
-                element={<NoMatch />} 
-              />
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/success" element={<Success />} />
+              <Route path="/orderHistory" element={<OrderHistory />} />
+              <Route path="/products/:id" element={<Detail />} />
+              <Route path="*" element={<NoMatch />} />
             </Routes>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,27 +1,27 @@
-import React, { createContext, useContext } from "react";
-// import { useProductReducer } from "./reducers";
-import { useDispatch, useSelector } from "react-redux";
+// import React, { createContext, useContext } from "react";
+// // import { useProductReducer } from "./reducers";
+// import { useDispatch, useSelector } from "react-redux";
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
+// const StoreContext = createContext();
+// const { Provider } = StoreContext;
 
-// const StoreProvider = ({ value = [], ...props }) => {
-//   const [state, dispatch] = useProductReducer({
-//     products: [],
-//     cart: [],
-//     cartOpen: false,
-//     categories: [],
-//     currentCategory: "",
-//   });
+// // const StoreProvider = ({ value = [], ...props }) => {
+// //   const [state, dispatch] = useProductReducer({
+// //     products: [],
+// //     cart: [],
+// //     cartOpen: false,
+// //     categories: [],
+// //     currentCategory: "",
+// //   });
 
-//   return <Provider value={[state, dispatch]} {...props} />;
+// //   return <Provider value={[state, dispatch]} {...props} />;
+// // };
+
+// const useStoreContext = () => {
+//   const dispatch = useDispatch();
+//   const state = useSelector((state) => state);
+
+//   return useContext(StoreContext);
 // };
 
-const useStoreContext = () => {
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state);
-
-  return useContext(StoreContext);
-};
-
-export { useStoreContext };
+// export { useStoreContext };
